2dea308e1ff4b01fd8dfcacbb4458a3f52a2c289
John Sherwood <jrsherwood@gmail.com>
2017-10-03 11:09:04 -0500

    conditional directives

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,16 +11,19 @@ import { Component } from '@angular/core';
     </ul>
     <hr>
     <div>
-      <h3>{{selectedTask.description}}</h3>
-      <p>Task Complete? {{selectedTask.done}}</p>
-      <h3> Edit Task</h3>
-      <label>Enter Task Description:</label>
-      <input [(ngModel)]="selectedTask.description">
-      <label>Enter Task Priority (1=3):</label>
-      <br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      <div *ngIf="selectedTask">
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete? {{selectedTask.done}}</p>
+        <h3> Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+        <label>Enter Task Priority (1=3):</label>
+        <br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)<br>
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
   `
@@ -35,7 +38,7 @@ export class AppComponent {
   tasks: Task[] = [new Task('Finish weekend Angular homework', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
@@ -57,6 +60,10 @@ export class AppComponent {
     } else {
       return "bg-info";
     }
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
