9b5eba100ac91f62871d3aad90f5d074a600c1cd
John Sherwood <jrsherwood@gmail.com>
2017-10-03 08:05:10 -0500

    intro to directives

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-      <li>{{firstTask.description}}</li>
+      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
     </ul>
   </div>
   `
@@ -19,7 +19,9 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  firstTask: Task = new Task("Finish weekend Angular homework");
+  tasks: Task[] = [new Task('Finish weekend Angular homework'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub')];
 }
 
 export class Task {
