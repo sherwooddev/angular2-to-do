1cfb91a580ae10e22216b924e33f9b3e0feed878
John Sherwood <jrsherwood@gmail.com>
2017-10-03 10:51:12 -0500

    two way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button></li>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask(currentTask)">Edit!</button></li>
     </ul>
+    <hr>
+    <div>
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3> Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1=3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+    </div>
   </div>
   `
 })
@@ -22,9 +35,10 @@ export class AppComponent {
   tasks: Task[] = [new Task('Finish weekend Angular homework', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)];
+  selectedTask: Task = this.tasks[0];
 
-  editTask() {
-    alert("Editing...");
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
 
   isDone(clickedTask: Task) {
