32e5727cc34221ca9e031d1972f12428f6703b93
John Sherwood <jrsherwood@gmail.com>
2017-10-08 12:53:31 -0500

    Data Down Actions Up

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Task } from './task.model';
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-    <task-list></task-list>
+    <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
     <hr>
     <div>
       <div *ngIf="selectedTask">
@@ -35,6 +35,11 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   selectedTask = null;
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,37 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
 
 @Component({
   selector: 'task-list',
   template: `
   <ul>
-    <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask(currentTask)">Edit!</button></li>
+    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}}  <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
   </ul>
   `
 })
 
 export class TaskListComponent {
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
 
-    isDone(clickedTask: Task) {
-      if(clickedTask.done === true) {
-        alert("Task is Complete");
-      } else {
-        alert("Task is NOT Complete");
-      }
-    }
+  editButtonHasBeenClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
 
-    priorityColor(currentTask) {
-      if (currentTask.priority === 3) {
-        return "bg-danger";
-      } else if (currentTask.priority === 2) {
-        return "bg-warning";
-      } else {
-        return "bg-info";
-      }
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("Task is Complete");
+    } else {
+      alert("Task is NOT Complete");
     }
+  }
+
+  priorityColor(currentTask) {
+    if (currentTask.priority === 3) {
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
