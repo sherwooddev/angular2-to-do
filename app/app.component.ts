cd47b309b6dd42dd42afce217cb4197db8b7ab63
John Sherwood <jrsherwood@gmail.com>
2017-10-08 13:33:22 -0500

    Template Reference Variables and creating objects

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,6 +10,7 @@ import { Task } from './task.model';
       <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
       <hr>
       <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+      <new-task (newTaskSender)="addTask($event)"></new-task>
     </div>
   `
 })
@@ -33,5 +34,9 @@ export class AppComponent {
 
   finishedEditing() {
     this.selectedTask = null;
+  }
+
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 }
