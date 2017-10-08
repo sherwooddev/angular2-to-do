64a3a55d45ac4996ba08005c27d0f52cb96b2c11
John Sherwood <jrsherwood@gmail.com>
2017-10-08 13:11:28 -0500

    Editing Models

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent,
-                  TaskListComponent],
+                  TaskListComponent,
+                EditTaskComponent],
   bootstrap: [ AppComponent ]
 })
 
